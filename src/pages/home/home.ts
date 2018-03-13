78393f8390e22449b9138d3dad069da370c3ff10
LuKaas <lukaas.blu@gmail.com>
2018-03-13 05:35:09 +0000

    Wrap everything up.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild, trigger, state, animate, transition, keyframes, style } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { trigger, state, animate, transition, keyframes, style } from '@angular/animations';
 import { NavController, Slides } from 'ionic-angular';
 
 import { MainPage } from '../main/main';
@@ -7,6 +8,15 @@ import { MainPage } from '../main/main';
     selector: 'page-home',
     templateUrl: 'home.html',
     animations: [
+
+        trigger('colorFade', [
+
+            transition('* => *', animate('700ms ease-out', keyframes([
+                style({ opacity: 0.6}),
+                style({ opacity: 0.8}),
+                style({ opacity: 1}),
+            ])))
+        ]),
 
         trigger('smoothSwipe', [
             state('*', style({
@@ -32,6 +42,8 @@ export class HomePage {
 
     state: string = 'x';
 
+    slideColor: string = "#00C677";
+
     constructor(public navCtrl: NavController) {
 
     }
@@ -44,6 +56,8 @@ export class HomePage {
         if (this.slides.isEnd())
             this.skipMsg = "Now, show me what you got !";
         else this.skipMsg = "Skip";
+
+        this.slideColor = this.setSlideColor(this.slides.getActiveIndex());
     }
 
     slideMoved() {
@@ -54,6 +68,19 @@ export class HomePage {
 
     animationDone() {
         this.state = 'x';
+    }
+
+    setSlideColor(slideIndex: number) {
+        console.log(slideIndex);
+
+        let slideColor: string;
+        switch(slideIndex) {
+            case 0: slideColor = "#00C677"; break;
+            case 1: slideColor = "#D1345B"; break;
+            default: slideColor = "#F4E409"; break; // meaning the last slide and more.
+        }
+
+        return slideColor;
     }
 
 }
